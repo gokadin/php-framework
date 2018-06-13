e0e71594f7668c950755d4e4973bb93c3bde007a
Givi Odikadze <givi.odikadze@grassvalley.com>
2018-06-12 23:43:11 -0400

    fix test

--- Tests/Library/Routing/RouteTest.php
+++ Tests/Library/Routing/RouteTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\LibraryRouting;
 
+use Library\Http\Request;
 use Library\Routing\Route;
 use Tests\BaseTest;
 
@@ -11,37 +12,46 @@ class RouteTest extends BaseTest
     {
         // Arrange
         $route = new Route(['GET'], '/test', 'controller', 'action', [], 'name1');
+        $request = new Request('GET', '/test');
 
         // Assert
-        $this->assertTrue($route->matches('GET', '/test'));
+        $this->assertTrue($route->matches($request));
     }
 
     public function test_match_withParameters()
     {
         // Arrange
         $route = new Route(['GET'], '/test', 'controller', 'action', [], 'name1');
+        $request1 = new Request('GET', '/test?one=abc');
+        $request2 = new Request('GET', '/some/test2?one=abc&two=def');
 
         // Assert
-        $this->assertTrue($route->matches('GET', '/test?one=abc'));
+        $this->assertTrue($route->matches($request1));
 
         // Act
         $route = new Route(['GET'], '/some/test2', 'controller', 'action', [], 'name1');
 
         // Assert
-        $this->assertTrue($route->matches('GET', '/some/test2?one=abc&two=def'));
+        $this->assertTrue($route->matches($request2));
     }
 
     public function test_match_withParametersWhenItShouldNotMatch()
     {
+        // Arrange
+        $request = new Request('GET', '/some/other?one=abc&two=def');
+
         // Act
         $route = new Route(['GET'], '/some/test', 'controller', 'action', [], 'name1');
 
         // Assert
-        $this->assertTrue(!$route->matches('GET', '/some/other?one=abc&two=def'));
+        $this->assertTrue(!$route->matches($request));
     }
 
     public function test_matchCatchAll_withDifferentUris()
     {
+        // Arrange
+        $request = new Request('GET', '/');
+
         // Act
         $route = new Route(['GET'], '/some/test', 'controller', 'action', [], 'name1');
 
