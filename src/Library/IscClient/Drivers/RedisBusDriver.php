c132a83ca64751ef3f478320954b4e44be528db3
Givi Odikadze <givi.odikadze@grassvalley.com>
2018-07-02 22:59:46 -0400

    cont

--- src/Library/IscClient/Drivers/RedisBusDriver.php
+++ src/Library/IscClient/Drivers/RedisBusDriver.php
@@ -70,7 +70,8 @@ class RedisBusDriver implements IBusDriver
 
     public function dispatch(string $channel, array $payload)
     {
-        $this->redis->publish($channel, json_encode($payload));
+        $x = $this->redis->publish($channel, json_encode($payload));
+        echo 'DISPATCH ON '.$channel.' CODE: '.$x.PHP_EOL;
     }
 
     public function listenToResult(string $channel)
